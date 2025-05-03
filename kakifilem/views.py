80deee6d4ab7f5b32fb8ee215b9f9b5d67ed2fa9
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-05-03 21:44:10 +0800

    Add files via upload

--- kakifilem/views.py
+++ kakifilem/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404, JsonResponse
 from django.core.cache import cache
 import logging
+from shortener.models import ShortURL
 
 logger = logging.getLogger(__name__)
 
@@ -10,3 +11,23 @@ def index(request):
 
 def countdown(request):
     return render(request, 'countdown.html')
+
+def redirect_to_original(request, short_id):
+    """Handle redirect for short URLs"""
+    try:
+        # Get the ShortURL object
+        short_url = ShortURL.objects.get(short_id=short_id)
+        
+        # Extract token and videoName from long_url if needed
+        if 'index.html' in short_url.long_url or 'token=' in short_url.long_url:
+            # Keep using long URL format for now
+            return redirect(short_url.long_url)
+        
+        # Increment access count
+        short_url.access_count += 1
+        short_url.save()
+        
+        return redirect(short_url.long_url)
+        
+    except ShortURL.DoesNotExist:
+        raise Http404("Short URL not found")
