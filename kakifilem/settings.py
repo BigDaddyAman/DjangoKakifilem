7f500f7ce2591a32c3242f286e693169c8d49587
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-05-21 22:39:53 +0800

    Add files via upload

--- kakifilem/settings.py
+++ kakifilem/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'kakifilem.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.getenv('DATABASE_PUBLIC_URL', 'postgresql://postgres:HqypeSBbxfHvqRacEPVihtFGHYUipEDC@gondola.proxy.rlwy.net:18020/railway'),
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'HqypeSBbxfHvqRacEPVihtFGHYUipEDC',
+        'HOST': 'gondola.proxy.rlwy.net',
+        'PORT': '18020',
+    }
 }
 
 
