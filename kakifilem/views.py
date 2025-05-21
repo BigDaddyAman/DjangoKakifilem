3800d2fb90b75591bcd3eeb100592480930fe554
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-05-21 22:14:40 +0800

    Add files via upload

--- kakifilem/views.py
+++ kakifilem/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.conf import settings
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def index(request):
     return render(request, 'index.html')
 
 def countdown(request):
     telegram_id = request.GET.get('telegram_id')
-    if not telegram_id:
-        # If no telegram_id, redirect to bot with token
-        token = request.GET.get('token')
-        if token:
-            return redirect(f'https://t.me/KakifilemBot?start={token}')
-            
+    token = request.GET.get('token')
+    video_name = request.GET.get('videoName')
+    
+    logger.info(f"Countdown request - Token: {token}, Video: {video_name}, Telegram ID: {telegram_id}")
+    
+    # Don't redirect immediately, always show countdown
     context = {
         'bot_api_url': settings.BOT_API_URL,
         'telegram_id': telegram_id,
