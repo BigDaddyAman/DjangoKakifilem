1aed9a5fc21d94e50cb99b9953cf7210f63bd914
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-06-20 07:57:56 +0800

    Delete miniapps directory

--- miniapps/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class MiniAppsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'miniapps'
-    verbose_name = 'Mini Applications'
